248d4343f5d30b954fd97fa9b97ffaf1d44dca18
samwilson155 <40063691+samwilson155@users.noreply.github.com>
2018-07-31 11:26:02 -0400

    Gang 4

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
+var PACount = 0
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /\bPA\b/;
+	  botRegex2 = /\bHeHe\b/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -16,12 +18,58 @@ function respond() {
     this.res.writeHead(200);
     this.res.end();
   }
+  
+    if(request.text && botRegex2.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+  
 }
 
 function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = 'The above Phi Alpha has been added to the jawn repository. Pa to all';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessage2() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'Goofy Ass';
 
   options = {
     hostname: 'api.groupme.com',
